a8cd85099f7d5eb6f4882709013100a534892c6d
Alex2019-1016 <132407910+Alex2019-1016@users.noreply.github.com>
2023-05-13 15:41:18 -0400

    Entidades del Proyecto

--- Data/Entities/Usuario.cs
+++ Data/Entities/Usuario.cs
@@ -2,5 +2,22 @@
 {
     public class Usuario
     {
+    
+    public int IdUsuario { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public int Edad { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
+    public string Contraseña { get; set; }
+    public string Genero { get; set; }
+    public string Ubicacion { get; set; }
+    public string FotoPerfil { get; set; }
+    
+    
+    
+    
+    
+    
     }
 }
