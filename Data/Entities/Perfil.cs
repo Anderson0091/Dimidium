0615eb267f67b11250b2f9abe8a9953ba716841f
Alex2019-1016 <132407910+Alex2019-1016@users.noreply.github.com>
2023-05-20 15:04:37 -0400

    Update Perfil.cs

--- Data/Entities/Perfil.cs
+++ Data/Entities/Perfil.cs
@@ -8,9 +8,11 @@ namespace Dimidiun.Data.Entities
     {
         [Key]
         public int IdPerfil { get; set; }
+        
         public int IdUsuario { get; set; }
         [ForeignKey("IdUsuario")]
         public Usuario Usuario { get; set; }
+        
         public string Descripcion { get; set; }
         public string Gustos { get; set; }
     }
