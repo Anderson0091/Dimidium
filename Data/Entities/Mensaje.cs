0b0a9a3435a7c3bc212e746a824be88cdb76f5ea
Anderson0091 <131827859+Anderson0091@users.noreply.github.com>
2023-05-30 11:29:28 -0400

    Update Mensaje.cs

--- Data/Entities/Mensaje.cs
+++ Data/Entities/Mensaje.cs
@@ -11,11 +11,11 @@ namespace Dimidiun.Data.Entities
         public int IdMensaje { get; set; }
 
         public int IdRemitente { get; set; }
-        [ForeignKey("IdRemitente")]
+       
         public Usuario Remitente { get; set; }
 
         public int IdDestinatario { get; set; }
-        [ForeignKey("IdDestinatario")]
+  
         public Usuario Destinatario { get; set; }
 
         public string ContenidoMensaje { get; set; }
