37df89992f558e16635693ca5547c23e39420c82
Anderson0091 <131827859+Anderson0091@users.noreply.github.com>
2023-05-30 11:30:06 -0400

    Update Cita.cs

--- Data/Entities/Cita.cs
+++ Data/Entities/Cita.cs
@@ -11,11 +11,11 @@ namespace Dimidiun.Data.Entities
         public int IdCita { get; set; }
 
         public int IdUsuario1 { get; set; }
-        [ForeignKey("IdUsuario1")]
+   
         public Usuario? Usuario1 { get; set; }
 
         public int IdUsuario2 { get; set; }
-        [ForeignKey("IdUsuario2")]
+    
         public Usuario? Usuario2 { get; set; }
 
         public DateTime FechaCita { get; set; }
