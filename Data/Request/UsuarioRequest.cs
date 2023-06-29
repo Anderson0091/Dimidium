06e48b5c5c4c0288a5069a75b56794b8b12b49a7
Anderson0091 <131827859+Anderson0091@users.noreply.github.com>
2023-06-29 14:15:18 -0400

    dique guarda

--- Data/Request/UsuarioRequest.cs
+++ Data/Request/UsuarioRequest.cs
@@ -4,14 +4,14 @@
     {
         public int IdUsuario { get; set; }
         public string Nombre { get; set; }
-        public string Apellido { get; set; }
+        public string? Apellido { get; set; }
         public int Edad { get; set; }
-        public string Telefono { get; set; }
-        public string Email { get; set; }
+        public string? Telefono { get; set; }
+        public string? Email { get; set; }
         public string Contraseña { get; set; }
-        public string Genero { get; set; }
-        public string Ubicacion { get; set; }
-        public string FotoPerfil { get; set; }
-        public string Intereses { get; set; }
+        public string? Genero { get; set; }
+        public string? Ubicacion { get; set; }
+        public string? FotoPerfil { get; set; }
+        public string? Intereses { get; set; }
     }
 }
